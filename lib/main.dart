231637317270ba7a0fd94dfd0397bb7ac6f0967c
gboliknow <gboliknow@gmail.com>
2021-04-25 13:27:26 -0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:shoe_ui/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.latoTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: HomePage(),
+    );
+  }
+}
